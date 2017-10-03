8fb82c4f33d1713b4fa0c10a0e8f7ce07319be4e
alu20487670y <alu2487670y@ieselcaminas.org>
2017-10-03 15:52:21 +0200

    Dialog of the exemple

--- app/src/main/java/org/ieselcaminas/francisco/dialogs/MainActivity.java
+++ app/src/main/java/org/ieselcaminas/francisco/dialogs/MainActivity.java
@@ -1,13 +1,66 @@
 package org.ieselcaminas.francisco.dialogs;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    CharSequence[] items = { "Google", "Apple", "Microsoft" };
+    boolean[] itemsChecked = new boolean [items.length];
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createDialog(0).show();
+            }
+        });
+    }
+
+    private Dialog createDialog(int id) {
+        switch (id) {
+            case 0:
+                return new AlertDialog.Builder(this)
+                        .setIcon(R.mipmap.ic_launcher)
+                        .setTitle("This is a dialog with some simple text...")
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {
+                                        Toast.makeText(getBaseContext(),
+                                                "OK clicked!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                        )
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {
+                                        Toast.makeText(getBaseContext(),
+                                                "Cancel clicked!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                        )
+                        .setMultiChoiceItems(items, itemsChecked,
+                                new DialogInterface.OnMultiChoiceClickListener() {
+                                    public void onClick(DialogInterface dialog,
+                                                        int which, boolean isChecked) {
+                                        Toast.makeText(getBaseContext(),
+                                                items[which] + (isChecked ? " checked!":" unchecked!"),
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                        ).create();
+        }
+        return null;
     }
 }
